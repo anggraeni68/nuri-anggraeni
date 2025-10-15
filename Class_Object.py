a300dc9a8e219854cd82e9cfc339088642b6d9ea
anggraeni68 <anggraeninuri205@gmail.com>
2025-10-15 20:16:34 +0700

    Add files via upload

---
+++ Class_Object.py
@@ -0,0 +1,45 @@
+#python 3.7.1
+
+class sekolah :
+    pass
+    
+class siswa :
+    pass
+    
+    
+class sekolah :
+    pass
+
+sekolah1 = sekolah()
+sekolah2 = sekolah ()
+
+print(type(sekolah1))
+print(type(sekolah2))
+
+
+class siswa :
+    pass
+
+siswa1 = siswa()
+siswa2 = siswa()
+
+print(type(siswa1))
+print(type(siswa2))
+
+
+#class
+
+class sekolah :
+    pass
+
+sekolah1 = sekolah()
+sekolah1.nama = "SMK PGRI 35 solokan jeruk" 
+sekolah1.alamat = "JL RHO kosasih No 90 cibodas" 
+sekolah2 = sekolah()
+sekolah2.nama = " SMK 9 bandung"
+sekolah2.alamat = " JL soekarno-hatta No 10"
+
+print(sekolah1.nama)
+print(sekolah1.alamat)
+print(sekolah2.nama)
+print(sekolah2.alamat)
