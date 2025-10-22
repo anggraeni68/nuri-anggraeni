e8458195db5fba32f5eb9979a01909a9b8fbb8e2
anggraeni68 <anggraeninuri205@gmail.com>
2025-10-22 14:35:49 +0700

    Add files via upload

---
+++ ClTugas1_Nuri anggraeni.py
@@ -0,0 +1,36 @@
+class alatmusik :
+    pass
+
+alatmusik1 = alatmusik()
+alatmusik2 = alatmusik()
+
+
+#class
+
+class alatmusik :
+    pass
+
+alatmusik1 = alatmusik()
+alatmusik1.nama = "angklung" 
+alatmusik1.asaldaerah = "jawabarat" 
+alatmusik1.caramemainkan = "digoyangkan"
+alatmusik1.jenis = "tradisional"
+alatmusik1.bahan = "bambu"
+
+alatmusik2 = alatmusik()
+alatmusik2.nama = "gendang"
+alatmusik2.caramemainkan = "dipukul"
+alatmusik2.asaldaerah = "jawabarat"
+alatmusik2.jenis = "tradisional"
+alatmusik2.bahan = "kulit dan kayu"
+
+print(alatmusik1.nama)
+print(alatmusik1.asaldaerah)
+print(alatmusik1.caramemainkan)
+print(alatmusik1.jenis)
+print(alatmusik1.bahan)
+print(alatmusik2.nama)
+print(alatmusik2.caramemainkan)
+print(alatmusik2.asaldaerah)
+print(alatmusik2.jenis)
+print(alatmusik2.bahan)
